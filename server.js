95271f52b8c868ba08125806da021a62a90ccd95
Dustin Lear <leard@oregonstate.edu>
2017-11-13 12:26:06 -0800

    required mongodb and added some express code

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 console.log("Server JavaScript start.");
 var fs = require('fs');
 //var http = require('http')
+var mongoDB = require('mongodb');
 var express = require('express');
-var app = express();
+var expressServer = express();
+
+
 
 
 //The default port for serve is 3117. However, the server will run with the port specified in the environment variable PORT if PORT is an environment variable
@@ -10,3 +13,16 @@ var port = 3117;
 if(process.env.PORT){
 	port = process.env.PORT;
 }
+expressServer.listen(port, function () {
+    /*
+	*
+	*
+	*insert callback code here
+	*
+	*
+	*/
+	console.log("---Server is listening on port ", port);
+
+});
+
+
