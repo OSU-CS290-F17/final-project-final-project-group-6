a8f6763de4681f88599b7de6da96eba12c2d1ecb
Dustin Lear <leard@oregonstate.edu>
2017-11-13 12:10:09 -0800

    Added initial code server.js and created a json package for it

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 console.log("Server JavaScript start.");
+var fs = require('fs');
+//var http = require('http')
+var express = require('express');
+var app = express();
+
+
+//The default port for serve is 3117. However, the server will run with the port specified in the environment variable PORT if PORT is an environment variable
+var port = 3117;
+if(process.env.PORT){
+	port = process.env.PORT;
+}
